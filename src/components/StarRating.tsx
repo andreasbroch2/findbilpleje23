4755438772c3232d99a6c39b8d1de587477e2806
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-07-27 02:22:56 +0200

    oin

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -5,18 +5,16 @@ export default function StarRating ({ placeName }) {
 
 
   const getStarRating = async (placeName) => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+
     
-    var data = await fetch("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=PD Bilpleje&inputtype=textquery&fields=rating%2Cname%2Cformatted_address%2Cuser_ratings_total&key=AIzaSyBGPS-pbI1-xxJCWzEyKhwD6DFRJldwMHg", requestOptions)
+    var data = await fetch("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=PD Bilpleje&inputtype=textquery&fields=rating%2Cname%2Cformatted_address%2Cuser_ratings_total&key=AIzaSyBGPS-pbI1-xxJCWzEyKhwD6DFRJldwMHg")
       .then(response => response.text())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
 
     return data;
   };
+  getStarRating(placeName);
   var name = '';
   var rating = 0;
   return (
